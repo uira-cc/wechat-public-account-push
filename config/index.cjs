82df384f1357f622ffddf30499cfc061234d532e
uira-cc <112848885+uira-cc@users.noreply.github.com>
2024-03-25 16:39:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -134,10 +134,6 @@ const USER_CONFIG = {
         {
           type: '节日', name: '结婚纪念日', year: '2023', date: '12-23',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1994', date: '01-16',
-        }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
@@ -170,15 +166,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1994', date: '02-25',
+          type: '生日', name: '老婆', year: '1994', date: '02-25',
         },
         {
           type: '节日', name: '结婚纪念日', year: '2023', date: '12-23',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1994', date: '01-16',
-        }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
@@ -259,13 +251,6 @@ const USER_CONFIG = {
     },
     {
       type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
-    },
-    // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-    {
-      type: '生日', name: '李四', year: '1996', date: '09-31', isShowAge: true,
-    },
-    {
-      type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
     }
     // 你可以不断按格式往下增加
     // ...
